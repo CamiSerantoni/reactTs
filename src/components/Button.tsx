52cfb763c4753302c4c8a56e3b0cf246e8499247
Camila Serantoni <camila.serantoni@blue.cl>
2024-12-10 22:20:43 -0300

    adding more features

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,21 @@
+import { Component, ComponentPropsWithoutRef } from "react";
 
 type ButtonProps = {
     el: "button"
-} 
+} & ComponentPropsWithoutRef<'button'>; 
 
 type AnchorProps = {
     el: "anchor"
-}
+} & ComponentPropsWithoutRef<'a'>;
 
 export default function Button(props: ButtonProps | AnchorProps) {
+    // const { el,...otherProps } = props;
+
+    if(props.el === "anchor"){
+        return <a {...props}>Click me</a>;
+    }
+    
     return <a>Click me</a>;
 
-    return <button>Click me</button>;
+    return <button {...props}>Click me</button>;
 }
