a24d711a386654c314dfc970c96f8b7fc1b3ee99
Camila Serantoni <camila.serantoni@blue.cl>
2024-12-13 23:57:36 -0300

    adding more about component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,9 +5,7 @@ type ContainerProps = {
   as: ElementType
 }
 
-export default  function Container = () => {
-  return (
-    <div>Container</div>
-  )
+export default  function Container({as}: ContainerProps) {
+  const Component  = as; 
+  return <As/>
 }
-
