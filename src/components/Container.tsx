a8676bf07b854d5e81f7e062e22ddb36dc85204c
Camila Serantoni <camila.serantoni@blue.cl>
2024-12-23 23:11:42 -0300

    building a polymorphic component part 1

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,11 @@
 import { type ReactNode, type ElementType, type ComponentPropsWithoutRef } from "react"
 
-type ContainerProps = {
-  as: ElementType
+type ContainerProps<T extends ElementType> = {
+  as?: T
   children: React.ReactNode
-} & ComponentPropsWithoutRef<>
+} & ComponentPropsWithoutRef<T>
 
-export default  function Container({as, children}: ContainerProps) {
-  const Component  = as; 
+export default  function Container<C extends ElementType>({as, children}: ContainerProps<C>) {
+  const Component  = as ||'div'; 
   return <Component> {children}</Component>
 }
