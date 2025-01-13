1ce5637d1bf0dbfd6a10e72bf705028f36e9f319
Camila Serantoni <camila.serantoni@blue.cl>
2025-01-13 05:45:01 -0300

    starting new project adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import Input from "./components/Input";
-// import Button from './components/Button';
-import Container from "./components/Container";
-import Button from "./components/Button";
-import { useRef } from "react";
-import Form, {type FormHandle} from "./components/Form";
-
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-const customForm = useRef<FormHandle>(null)
-
-  function handleSave(data: unknown){
-
-    const extractedData= data as {name: string, age:string}
-    console.log(extractedData)
-    customForm.current?.clear()
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}> 
-        <Input type="text" label="name" id="name"/>
-        <Input type="number" label="age" id="age"/>
-        <p> 
-          <Button>Save</Button>
-        </p>
-        </Form>
-      {/* <Input label="Test" id="test" ref={input}/> */}
-      {/* <Container as={Button}>Click me</Container>{" "} */}
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
