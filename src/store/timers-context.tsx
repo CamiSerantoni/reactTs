015d2ae7f2621093476bc794020d710e09181434
Camila Serantoni <camila.serantoni@blue.cl>
2025-01-13 06:41:38 -0300

    getting started with useReducer part 2

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,5 +1,5 @@
-import { type ReactNode, createContext } from "react";
-import Timers from '../components/Timers';
+import { type ReactNode, createContext, useContext, useReducer, useState } from "react";
+
 
 type Timer ={
     name: string;
@@ -12,6 +12,11 @@ type TimerState = {
     timers: Timer[]
 }
 
+const initialState: TimerState = {
+    isRunning: true,
+    timers: []
+}
+
 type TimersContextValue = TimerState & {
     addTimer: (timerData: Timer) => void,
     starTimers: () => void,
@@ -20,14 +25,29 @@ type TimersContextValue = TimerState & {
 
 const TimersContext = createContext<TimersContextValue | null>(null)
 
+/*  This one will be a custom hook: in the end  is a regular function that starts with "use"  */
+export function useTimersContext (){
+   const timersCtx = useContext(TimersContext)
+
+
+if (timersCtx === null ){
+    throw new Error('TimersContext is null - that should not be the case')
+}
+return timersCtx
+}
+
+
 type TimersContextProviderProps = {
     children: ReactNode
 }
 
 export default function TimersContextProvider({children}: TimersContextProviderProps ){
+    useReducer(reducer, initialState)
+   
+   
     const ctx: TimersContextValue = {
         timers: [], 
-        isRunning: false,
+        isRunning: true,
         addTimer(timerData: Timer){
 
         },
