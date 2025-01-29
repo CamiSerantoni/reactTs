1ea794824aa54bac7af9ce3f953903eaeab16197
Camila Serantoni <camila.serantoni@blue.cl>
2025-01-28 23:37:14 -0300

    adding http helper

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import AddTimer from './components/AddTimer.tsx';
 import Header from './components/Header.tsx';
 import Timers from './components/Timers.tsx';
 import TimersContextProvider from './store/timers-context.tsx';
+import { get } from './util/http.ts';
 
 function App() {
+  get('https://jsonplaceholder.typicode.com/posts')
   return (
     <TimersContextProvider>
       <Header />
