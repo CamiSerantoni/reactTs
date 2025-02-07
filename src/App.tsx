c735a8922d710da0e68049b1548b951ee48d43f4
Camila Serantoni <camila.serantoni@blue.cl>
2025-02-07 00:52:35 -0300

    adding fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import AddTimer from './components/AddTimer.tsx';
 import Header from './components/Header.tsx';
 import Timers from './components/Timers.tsx';
 import TimersContextProvider from './store/timers-context.tsx';
 import { get } from './util/http.ts';
+import { BlogPost } from './components/BlogPosts.tsx';
 
 function App() {
-  get('https://jsonplaceholder.typicode.com/posts')
+
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>()
+
+
+  useEffect(()=> {
+
+async function  fetchPosts(){
+  await get('https://jsonplaceholder.typicode.com/posts')
+}
+
+fetchPosts()
+  },[])
+ 
   return (
     <TimersContextProvider>
       <Header />
