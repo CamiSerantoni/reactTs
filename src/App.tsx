2867f9c2632b44f82a04d7cd92ebd77bebd0e795
Camila Serantoni <camila.serantoni@blue.cl>
2025-03-08 23:59:31 -0300

    upcomming

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from 'react';
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import Timers from './components/Timers.tsx';
-import TimersContextProvider from './store/timers-context.tsx';
-import { get } from './util/http.ts';
-import { BlogPost } from './components/BlogPosts.tsx';
+import { useEffect, useState } from "react";
+// import AddTimer from './components/AddTimer.tsx';
+// import Header from './components/Header.tsx';
+// import Timers from './components/Timers.tsx';
+import TimersContextProvider from "./store/timers-context.tsx";
+import { get } from "./util/http.ts";
+import { BlogPost } from "./components/BlogPosts.tsx";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
 
-  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>()
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
+    }
 
+    fetchPosts();
+  }, []);
 
-  useEffect(()=> {
-
-async function  fetchPosts(){
-  const data = await get('https://jsonplaceholder.typicode.com/posts')
-}
-
-fetchPosts()
-  },[])
- 
-  return (
-    <TimersContextProvider>
-      <Header />
-      <main>
-        <AddTimer />
-        <Timers />
-      </main>
-    </TimersContextProvider>
-  );
+  return <h1>Data Fetching</h1>;
 }
 
 export default App;
